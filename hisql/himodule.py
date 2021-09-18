e3dd5879c26bc4997baa65ce3e21b2c5219ffc32
luoguochun <450171094@qq.com>
2021-09-18 10:33:21 +0800

    增加批量sql

--- hisql/himodule.py
+++ hisql/himodule.py
@@ -1,8 +1,10 @@
 import random
 import string
+from inspect import isfunction
 
 from pugsql.compiler import Module
 from pugsql.statement import Statement
+from pugsql.statement import Result
 
 
 class HiModule(Module):
@@ -16,7 +18,51 @@ class HiModule(Module):
         self.sqlpaths |= set(new_paths)
 
     def execute(self, sql, result, *multiparams, **params):
+        """
+
+        :param sql:  sql语句
+        :param result: 返回的结果类型, One, Many, Insert, Affected, Raw
+        :param multiparams: sql参数
+        :param params: sql参数
+        :return: 根据result返回的结果
+        """
         name = ''.join(random.sample(string.ascii_letters + string.digits, 10))
         stmt = Statement(name=name, sql=sql, result=result, doc='', filename=None)
         stmt.set_module(self)
         return stmt(*multiparams, **params)
+
+    def prepare(self, *sqls):
+        for it in sqls:
+            name, sql, result = None, None, None
+            if isinstance(it, dict):
+                keys = it.keys()
+                if 'name' not in keys or 'sql' not in keys or 'result' not in keys:
+                    raise ValueError(
+                        'sql dict {} - should have the following key: name, sql, result'.format(it))
+                name, sql, result = it['name'], it['sql'], it['result']
+            if isinstance(it, list) or isinstance(it, tuple):
+                if len(it) < 3:
+                    raise ValueError(
+                        'sql seq {} - should have the at least 3 items'.format(it))
+                name, sql, result = it[0], it[1], it[2]
+
+            if name is None:
+                raise ValueError(
+                    'arguments should be dict/list/tuple'.format(it))
+
+            if not isinstance(result, Result):
+                result = result()
+
+            stmt = Statement(name=name, sql=sql, result=result, doc='', filename=None)
+            if hasattr(self, name):
+                if name not in self._statements:
+                    raise ValueError(
+                        'Error the function name "{}" is '
+                        'reserved. Please choose another name.'.format(s.name))
+
+                raise ValueError('Error a SQL function named {} was '
+                                 'already defined in.'.format(name))
+
+            stmt.set_module(self)
+            setattr(self, name, stmt)
+            self._statements[name] = stmt

3fb2a0532ba93965f45db93588ea52e1ed1e1a56
luoguochun <450171094@qq.com>
2023-02-13 15:35:22 +0800

    bug fixed

--- setup.py
+++ setup.py
@@ -1,14 +1,26 @@
 from setuptools import find_packages, setup
 
+with open("README.MD", "r") as f:
+    long_description = f.read()
+
 setup(
     name='hisql',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(include=['hisql']),
     include_package_data=True,
     zip_safe=False,
+    description='纯SQL操作数据库',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/buf1024/hisql",
     platform="any",
     install_requires=[
         'pugsql',
         'pandas'
     ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
 )
